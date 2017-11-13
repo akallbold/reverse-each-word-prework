ce1888df65fe599d53427cbfe97db4d9d3544d86
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-11-13 21:59:48 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,17 @@
 # require 'pry'
 
 def reverse_each_word(string)
-  output=""
   array=string.split(" ")
   array.each do |word|
-    word_length=word.length
-    word_length.times do
-      counter=0
-     output+= word.chars.last
-
+    word.reverse
+    # word_length=word.length
+    # word_length.times do
+    #   counter=0
+    #  output+= word.chars.last
+     
     # binding.pry
     end
-    output+=" "
+    output= array.join(" ")
   end
   output
 end
